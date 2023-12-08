6855014274861d898f29f91c5af8fcb65c4f6502
Saurabey ?? <saurabbaral03@gmail.com>
2023-12-09 00:03:51 +0545

    auto detection and auto correction completed

--- hell.cpp
+++
@@ -1,9 +0,0 @@
-// write the cpp code to print("Hello World!")
-
-#include <iostream>
-using namespace std;
-
-int main() {
-    cout << "Hello World!" << endl;
-    return 0;
-}
--- nepali_text.py
+++ nepali_text.py
@@ -1,12 +1,14 @@
-word = "प ए न्स इ ल"
+import pickle
+
+
+# word = "प ए न्स इ ल"
 # word = 'खाइनछ'
 # word = ''
-word = 'अ क अ स'
+# word = 'अ क अ श'
+
+word = input("Enter a word: ")
 
 # word = 'ख उ क उ र ई'
-
-
-
 
 # for letters in word:
 #     print(letters, end=' ')
@@ -42,27 +44,28 @@ for i, letter in enumerate(word):
         new_word += letter
 
 print(new_word)
-    
-with open('generated_nepali_text.txt', 'w', encoding='utf-8') as f :
-    f.write(new_word)
 
-# remove space from the word
-# for words in dict_1:
-    
-#     new_word = word.replace(words, dict_1[words])
+with open('knn_model.pkl', 'rb') as f:
+    model = pickle.load(f)
 
+with open('nepali_dataset.txt', 'r', encoding='utf-8') as f:
+    data = f.read()
+    words = data.split()
 
-# print(new_word)
-# print(new_word)
-
-# print(new_word)
+letters = sorted(set(''.join(words)))
+letter_to_index = {letter: idx for idx, letter in enumerate(letters)}
 
 
-# #khukuri = "खुकुरी"
-# word = "खुकुरी"
+input_letter_representation = [0] * len(letters)
+for letter in new_word:
+    if letter in letter_to_index:
+        input_letter_representation[letter_to_index[letter]] += 1
 
+# Find nearest neighbors based on letter-level representations
+distances, indices = model.kneighbors([input_letter_representation])
 
+# Print nearest words
+nearest_words = [words[idx] for idx in indices.flatten()]
+with open("generated_nepali_text.txt", "w", encoding='utf-8') as f:
+    f.write(f'{new_word} : {nearest_words}')
 
-# # Iterate through each character in the word
-# for char in word:
-#     print(char, end=' ')
